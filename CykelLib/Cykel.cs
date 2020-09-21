282874a6b27dc887b2bf9bf2125ef753ef7f6f99
Morten Jakobsen Møller <mort748a@edu.easj.dk>
2020-09-21 10:23:12 +0200

    Add project files.

---
+++ CykelLib/Cykel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CykelLib
+{
+    public class Cykel
+    {
+        private int _id;
+        private string _farve;
+        private int _pris;
+        private int _gear;
+
+        public Cykel()
+        {
+        }
+
+        public Cykel(int id, string farve, int pris, int gear)
+        {
+            Id = id;
+            Farve = farve;
+            Pris = pris;
+            Gear = gear;
+        }
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Farve
+        {
+            get => _farve;
+            set
+            {
+                if (value.Length < 1) throw new ArgumentOutOfRangeException("Skal være mindst 1 tegn");
+                _farve = value;
+            }
+        }
+
+        public int Pris
+        {
+            get => _pris;
+            set
+            {
+                if (value <= 0) throw new ArgumentOutOfRangeException("Skal være større end 0");
+                _pris = value;
+            }
+        }
+
+        public int Gear
+        {
+            get => _gear;
+            set
+            {
+                if (value < 3 || value > 32) throw new ArgumentOutOfRangeException("Skal være mellem 3 og 32");
+                _gear = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+}
---
+++ CykelLibTests/CykelTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CykelLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CykelLib.Tests
+{
+    [TestClass()]
+    public class CykelTests
+    {
+
+        private Cykel cykel;
+
+        [TestInitialize]
+        public void Init()
+        {
+            cykel = new Cykel();
+        }
+        [TestMethod()]
+        public void CykelFarveTest()
+        {
+            cykel.Farve = "rød";
+            Assert.ThrowsException<ArgumentNullException>(() => cykel.Farve = null);
+            Assert.AreEqual("rød", cykel.Farve);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => cykel.Farve = "" );
+        }
+
+        [TestMethod()]
+        public void CykelPrisTest()
+        {
+            cykel.Pris = 100;
+            Assert.AreEqual(100, cykel.Pris);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(()=> cykel.Pris = 0);
+        }
+        [TestMethod()]
+        public void CykelGearTest()
+        {
+            cykel.Gear = 3;
+            Assert.AreEqual(3, cykel.Gear);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(()=> cykel.Gear = 2);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(()=> cykel.Gear = 33);
+        }
+
+        //[TestMethod()]
+        //public void ToStringTest()
+        //{
+        //    Assert.Fail();
+        //}
+    }
+}
